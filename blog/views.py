9c053429d99491d5691ca601a24edc310deeb7b9
OUYA77 <dhfz1214@gmail.com>
2023-10-10 16:24:56 +0900

    2nd commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.shortcuts import render, get_object_or_404
 
+from rest_framework import viewsets
+from .serializers import PostSerializer 
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+class IntruderImage(viewsets.ModelViewSet):
+    queryset = Post.objects.all() 
+    serializer_class = PostSerializer
--- mysite/settings.py
+++ mysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'blog',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,12 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "meida/"
+
+REST_FRAMEWORK={
+    'DEFAULT_PERMISSION_CLASSES':[
+    ],
+    'PAGE_SIZE' : 10
+}
